afa132bd6fcd0a6b22dd04936445c8f716d01d6f
Sakharov Denys <sakharov92@gmail.com>
2020-08-23 20:51:44 +0300

    add bloks

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import "../css/Promo.css"
+import background from "../img/background.png"
 
 
 function Promo() {
+
+    function slideLeft() {
+        console.log("left")
+    }
+
+
+
+    function slideRight() {
+        console.log("right")
+    }
+
+
     return (
-        <div className="wrapper"></div>
+        <div className="promo">
+            <div className="sliderBlock">
+                <div className="sliderPicture">
+                    <img src={background} alt="background"></img>
+                </div>
+                <div className="promoTitle">Pure CBD Pure Results</div>
+                <div className="buttonBlock">
+                    <div className="buttonBlockLeft" onClick={slideLeft}><i className="fas fa-arrow-left"></i></div>
+                    <div className="buttonBlockRight" onClick={slideRight}><i className="fas fa-arrow-right"></i></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
